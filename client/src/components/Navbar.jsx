1de2c7d8c5bbde4f3cc23b625f8b75b2af7eaf32
StanleyKioko <mwangestanley02@gmail.com>
2025-08-27 22:49:48 +0300

    Added  login Button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.svg'
 import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
+import {useClerk, useUser, UserButton} from '@clerk/clerk-react'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const {user} = useUser()
+  const {openSignIn} = useClerk()
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5 backdrop-blur">
       <Link to="/" className="max-md:flex-1">
@@ -32,7 +35,17 @@ const Navbar = () => {
 
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+        {
+          !user ?(
+            <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary
+         hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+
+          ) : (
+            <UserButton />
+          )
+        }
+        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary
+         hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Logout</button>
       </div>
 
       {/* Hamburger Icon for Mobile */}
